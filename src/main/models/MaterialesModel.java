3c0ce915f3bc9a5b5a6557c6755ca68b99517196
Daniela9903 <rosas.tome@gmail.com>
2022-02-16 12:55:57 -0600

    Nuevo archivo

--- src/main/models/MaterialesModel.java
+++ src/main/models/MaterialesModel.java
@@ -1,7 +1,12 @@
-package com.example.apiPropia.models;
-import javax.persistence.*;
 
+import static org.mockito.ArgumentMatchers.nullable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="sucursal")
